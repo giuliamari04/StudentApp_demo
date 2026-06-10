2129a7de9d774f598d5c90dee6d054e8fe93b4e7
giuliamari04 <giuliamari04@gmail.com>
2026-06-10 11:27:06 +0200

    fix deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ function Footer() {
 
   return (
     <div>
-      footer
+      <footer className="border-t border-slate-600 py-6">
+        <p className="text-slate-400 text-center">
+          © 2023 Student App. Tutti i diritti riservati.
+        </p>
+      </footer>
     </div>
   )
 }
